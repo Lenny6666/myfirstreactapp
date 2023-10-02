a318bc52e56ae6b5ce57d4f903aa2fe9176640f3
lenny6666 <shavjo.gamer@gmail.com>
2023-10-02 18:50:14 +0200

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Codecademy!
+          Hello Codecademy and hello Me!
         </p>
         <a
           className="App-link"
